949d651209c8d9de84052985175e5fb946541105
mkm5 <58401949+mkm5@users.noreply.github.com>
2026-02-08 00:50:39 +0100

    user deletion, tickets better archived display

--- src/Controller/Admin/Users/UsersController.php
+++ src/Controller/Admin/Users/UsersController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin\Users;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsCsrfTokenValid;
 
 final class UsersController extends AbstractController
 {
@@ -18,5 +22,18 @@ final class UsersController extends AbstractController
             'listing' => 'admin/_users.html.twig',
             'entityClassName' => User::class,
         ]);
+    }
+
+    #[Route('/admin/users/{id}/delete', name: 'app_admin_user_delete', methods: 'POST')]
+    #[IsCsrfTokenValid(new Expression('"delete-user-" ~ args["user"].getId()'))]
+    public function delete(User $user, #[CurrentUser] User $currentUser, EntityManagerInterface $em): Response
+    {
+        if ($user->getId() === $currentUser->getId()) {
+            throw $this->createAccessDeniedException('You cannot delete your own account.');
+        }
+
+        $em->remove($user);
+        $em->flush();
+        return $this->redirectToRoute('app_admin_users');
     }
 }
